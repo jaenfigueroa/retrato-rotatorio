76c5df502ee1e235194e55305ec0a6c30cb53513
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-10-17 02:11:45 -0500

    list

---
+++ README.md
@@ -0,0 +1,7 @@
+# <div align="center">touchstart, touchmove, touchend 🚀</div>
+
+# <div align="center">colisiones🚀</div>
+
+## 🔗 Vista previa en Desktop
+
+![App Screenshot](./assets/captura.png)
--- main.js
+++ main.js
@@ -1,25 +1,86 @@
-const elemento = document.querySelector('#elemento')
-const caja = document.querySelector('#caja')
+const elemento = document.getElementById('X55')
+const texto = document.querySelector('#texto')
 ////////////////////////////////////////////////////////
-// elemento.addEventListener('dragstart', () => {
-//   console.log('se empezo a arrastrar');
-// })
-//////////////////
-// caja.addEventListener('dragover', (evento) => {
-//   evento.preventDefault()
-// })
+let ID_GUARDADO
 
-// caja.addEventListener('drop', (evento) => {
-//   console.log('se solto');
-// })
-
-////////////////////////////////////////////////////////
 elemento.addEventListener('touchmove', (evento) => {
-  // console.log(evento.target);
-  // console.log(evento.targetTouches);
-  // console.log(evento.targetTouches[0]);
-  let ubicacion = evento.targetTouches[0]
-  elemento.style.left = ubicacion.pageX + 'px'
-  elemento.style.top = ubicacion.pageY + 'px'
 
+  let ancho = elemento.getBoundingClientRect().width
+
+  ID_GUARDADO = evento.target.id
+
+  /* coordenadas del elemento para moverlo*/
+  let elementoX = evento.targetTouches[0].pageX
+  let elementoY = evento.targetTouches[0].pageY
+
+  // console.log(elementoX, elementoY);
+
+  //RESTRINGIR MOVER DE MAS //////////
+  anchoPantalla = window.innerWidth
+  altoPantalla = window.innerHeight
+
+  if (elementoX < anchoPantalla - ancho / 2 && elementoX > ancho / 2) {
+    elemento.style.left = elementoX - ancho / 2 + 'px'
+  }
+
+  if (elementoY < altoPantalla - ancho / 2 && elementoY > ancho / 2) {
+    elemento.style.top = elementoY - ancho / 2 + 'px'
+  }
 })
+
+elemento.addEventListener('touchend', (evento) =>
+
+  comprobarColision(ID_GUARDADO)
+)
+
+///////////////////////////////////////////////////
+//ESTRUCTURA CLASE/////////////////////////////////
+class Objeto {
+  constructor(X, Y, alto, ancho) {
+    this.arriba = Y
+    this.abajo = Y + alto
+    this.derecha = X + ancho
+    this.izquierda = X
+  }
+}
+
+///CREAR OBJETOS///////////////////////////////////
+function crearObjeto(id) {  //ID
+
+  let elemento = document.getElementById(id).getBoundingClientRect()
+
+  let x = elemento.x
+  let y = elemento.y
+  let ancho = elemento.width
+  let alto = elemento.height
+
+  ///////////////
+  return new Objeto(x, y, alto, ancho)
+}
+///COMPROBAR COLISION//////////////////////////////
+function comprobarColision(id) { //OBJETO, OBJETO
+
+  let elemento = crearObjeto(id)
+  let caja = crearObjeto(`lugar${id}`)
+
+  if (
+    elemento.abajo < caja.arriba ||
+    elemento.arriba > caja.abajo ||
+    elemento.derecha < caja.izquierda ||
+    elemento.izquierda > caja.derecha
+  ) {
+    console.log('NO Hay colision');
+    // console.log('NO ES AHI 😅');
+    texto.textContent = 'NO'
+
+  } else {
+    console.log('SI hay colision');
+    // console.log('CORRECTO 😀');
+    texto.textContent = 'SI'
+  }
+}
+
+///PONER EN PANTALLA COMPLETA///////////////////
+function aver() {
+  document.getElementById('main').requestFullscreen()
+}
