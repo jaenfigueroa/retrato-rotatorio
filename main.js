cbf1dc205570c2b4f9f79f817aca5fc70359c898
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-11-03 13:39:24 -0500

    mover en pantalla tactil terminado

--- main.js
+++ main.js
@@ -1,86 +1,15 @@
-const elemento = document.getElementById('X55')
-const texto = document.querySelector('#texto')
+const elemento = document.getElementById('elemento')
 ////////////////////////////////////////////////////////
-let ID_GUARDADO
 
 elemento.addEventListener('touchmove', (evento) => {
 
-  let ancho = elemento.getBoundingClientRect().width
+  let anchoElemento = elemento.getBoundingClientRect().width
 
-  ID_GUARDADO = evento.target.id
-
-  /* coordenadas del elemento para moverlo*/
+  /* COORDENADAS DEL ELEMENTO*/
   let elementoX = evento.targetTouches[0].pageX
   let elementoY = evento.targetTouches[0].pageY
 
-  // console.log(elementoX, elementoY);
-
-  //RESTRINGIR MOVER DE MAS //////////
-  anchoPantalla = window.innerWidth
-  altoPantalla = window.innerHeight
-
-  if (elementoX < anchoPantalla - ancho / 2 && elementoX > ancho / 2) {
-    elemento.style.left = elementoX - ancho / 2 + 'px'
-  }
-
-  if (elementoY < altoPantalla - ancho / 2 && elementoY > ancho / 2) {
-    elemento.style.top = elementoY - ancho / 2 + 'px'
-  }
+  /* MOVERLO DE LUGAR */
+  elemento.style.left = elementoX - anchoElemento / 2 + 'px'
+  elemento.style.top = elementoY - anchoElemento / 2 + 'px'
 })
-
-elemento.addEventListener('touchend', (evento) =>
-
-  comprobarColision(ID_GUARDADO)
-)
-
-///////////////////////////////////////////////////
-//ESTRUCTURA CLASE/////////////////////////////////
-class Objeto {
-  constructor(X, Y, alto, ancho) {
-    this.arriba = Y
-    this.abajo = Y + alto
-    this.derecha = X + ancho
-    this.izquierda = X
-  }
-}
-
-///CREAR OBJETOS///////////////////////////////////
-function crearObjeto(id) {  //ID
-
-  let elemento = document.getElementById(id).getBoundingClientRect()
-
-  let x = elemento.x
-  let y = elemento.y
-  let ancho = elemento.width
-  let alto = elemento.height
-
-  ///////////////
-  return new Objeto(x, y, alto, ancho)
-}
-///COMPROBAR COLISION//////////////////////////////
-function comprobarColision(id) { //OBJETO, OBJETO
-
-  let elemento = crearObjeto(id)
-  let caja = crearObjeto(`lugar${id}`)
-
-  if (
-    elemento.abajo < caja.arriba ||
-    elemento.arriba > caja.abajo ||
-    elemento.derecha < caja.izquierda ||
-    elemento.izquierda > caja.derecha
-  ) {
-    console.log('NO Hay colision');
-    // console.log('NO ES AHI 😅');
-    texto.textContent = 'NO'
-
-  } else {
-    console.log('SI hay colision');
-    // console.log('CORRECTO 😀');
-    texto.textContent = 'SI'
-  }
-}
-
-///PONER EN PANTALLA COMPLETA///////////////////
-function aver() {
-  document.getElementById('main').requestFullscreen()
-}
